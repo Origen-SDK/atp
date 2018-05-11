097f14ad59519097d21da8f8a2d989ef68f4137a
Stephen McGinty <stephen.mcginty@nxp.com>
2018-05-11 11:46:54 +0100

    Put more test name data in marshaled AST

--- lib/atp/processors/marshal.rb
+++ lib/atp/processors/marshal.rb
@@ -5,16 +5,24 @@ module ATP
       def on_object(node)
         o = node.value
         if o.is_a?(String)
-          node.updated(nil, [{ 'Test' => o }])
+          meta = { 'Test' => o }
         elsif o.is_a?(Hash)
-          node.updated(nil, [o])
+          meta = o
         elsif o.respond_to?(:to_meta) && o.to_meta && !o.to_meta.empty?
-          node.updated(nil, [o.to_meta])
+          meta = o.to_meta
         else
-          meta = { 'Test' => o.name }
-          meta['Pattern'] = o.try(:pattern)
-          node.updated(nil, [meta])
+          meta = {}
         end
+        # The test suite / test instance name
+        meta['Test'] ||= o.try(:name)
+        meta['Pattern'] ||= o.try(:pattern)
+        # The test name column on IG-XL, or the name of a specific instance of a test which shares a common
+        # 'Test' name with other tests
+        meta['Test Name'] ||= o.try(:test_name) || o.try(:_test_name) || o.try('TestName') || meta['Test']
+        # The name of the primary test that is logged by the test instance / test method, if it logs more
+        # than one then this is represented by sub_test nodes
+        meta['Sub Test Name'] ||= o.try(:sub_test_name) || o.try('SubTestName') || meta['Test']
+        node.updated(nil, [meta])
       end
 
       def on_render(node)
